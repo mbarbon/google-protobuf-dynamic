852996499b938e6c28ae4cf39e4ddfea9e4a549d
Mattia Barbon <mattia@barbon.org>
2015-12-31 22:23:37 +0100

    Add an option to compile with debug information

--- inc/GPD/Build.pm
+++ inc/GPD/Build.pm
@@ -6,16 +6,23 @@ use parent 'Module::Build::WithXSpp';
 
 use Alien::ProtoBuf;
 use Alien::uPB;
+use Getopt::Long;
+
+# yes, doing this in a module is ugly; OTOH it's a private module
+GetOptions(
+    'g'         => \my $DEBUG,
+);
 
 sub new {
     my $class = shift;
+    my $debug_flag = $DEBUG ? ' -g' : '';
     my $self = $class->SUPER::new(
         @_,
         extra_typemap_modules => {
             'ExtUtils::Typemaps::STL::String' => '0',
         },
         extra_linker_flags => [Alien::uPB->libs, Alien::ProtoBuf->libs],
-        extra_compiler_flags => [Alien::uPB->cflags, Alien::ProtoBuf->cflags],
+        extra_compiler_flags => [$debug_flag, Alien::uPB->cflags, Alien::ProtoBuf->cflags],
     );
 
     return $self;
