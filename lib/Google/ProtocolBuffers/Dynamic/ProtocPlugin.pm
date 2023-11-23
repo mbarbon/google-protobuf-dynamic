4ecd25279843964d516a6567db0e25e23c2321ed
Mattia Barbon <mattia@barbon.org>
2023-11-23 22:31:51 +0100

    Add --help and --man to protoc plugins
    Fixes #45

--- lib/Google/ProtocolBuffers/Dynamic/ProtocPlugin.pm
+++ lib/Google/ProtocolBuffers/Dynamic/ProtocPlugin.pm
@@ -6,6 +6,31 @@ use Google::ProtocolBuffers::Dynamic::ProtocInterface;
 
 sub import {
     my ($class, %args) = @_;
+
+    if (@ARGV && grep /^(?:--help|-h)$/, @ARGV) {
+        my $message = 'This is a protoc plugin and should not be used directly. Use --man for documentation';
+
+        if ('podusage' eq ($args{help} // '')) {
+            require Pod::Usage;
+
+            Pod::Usage::pod2usage(
+                -message => $message . "\n",
+                -exitval => 1,
+            );
+        } else {
+            print STDERR $message, "\n\n";
+
+            exit 1;
+        }
+    } elsif (@ARGV && grep /^--man$/, @ARGV) {
+        require Pod::Usage;
+
+        Pod::Usage::pod2usage(
+            -exitval => 0,
+            -verbose => 2,
+        );
+    }
+
     my $generator = $args{run};
     eval "require $generator; 1"
         or die "Error loding plugin '$generator': $@";
